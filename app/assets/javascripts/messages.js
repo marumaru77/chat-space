a971377a0641d0e0c2add639146921f56425153d
marumaru77 <marumaru07unchain@gmail.com>
2019-08-13 16:51:28 +0900

    Update messages.js

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -40,7 +40,6 @@ $(function(){
   var reloadMessages = function() {
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
       var last_message_id =  $('.message:last').data("message-id");
-      console.log(last_message_id);
       $.ajax({
         url: "api/messages",
         type: 'get',
